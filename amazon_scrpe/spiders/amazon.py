fb775c0f86bc8d81896da9b3a19128197f019428
akashSharma135 <akashsharma1352000@gmail.com>
2021-10-28 11:04:30 +0530

    second commit

--- amazon_scrpe/spiders/amazon.py
+++ amazon_scrpe/spiders/amazon.py
@@ -5,22 +5,32 @@ from amazon_scrpe.urls import url_list
 import urllib
 
 class AmazonCrawlSpider(CrawlSpider):
+
     name = 'amazon_crawl'
     allowed_domains = ['www.amazon.in']
     start_urls = url_list
     
+    # Extract category link
     mobile_category_rule = Rule(LinkExtractor(restrict_css='ul > .a-spacing-micro a '), follow=True)
+
+    # Extract details
     mobile_detail_rule = Rule(LinkExtractor(restrict_css='.rush-component > a'), callback='parse_item', follow=False)
+
+    # Extract next page link
     next_page_rule = Rule(LinkExtractor(restrict_css='.a-last a'), follow=True)
 
+    # Rules to scrape the pages
     rules = (
         mobile_category_rule,
         mobile_detail_rule,
         next_page_rule
     )
 
+    # Parsing the scraped data
     def parse_item(self, response):
         items = AmazonScrpeItem()
+
+        # getting the type of product searched
         url = response.url
         parsed_url = urllib.parse.urlparse(url)
         product_type = urllib.parse.parse_qs(parsed_url.query)['keywords']
@@ -32,6 +42,7 @@ class AmazonCrawlSpider(CrawlSpider):
         
         lis = response.css('#feature-bullets > ul > li')
         data = []
+        # append the feature li in data list
         for li in lis:
             data.append(li.css('span::text').get().strip())
             
